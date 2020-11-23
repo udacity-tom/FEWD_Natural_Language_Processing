f24be3ab529abf7b5ab4d189798551b5a114383e
udacity-tom <udacity@iamtom.com>
2020-11-23 23:26:27 +0100

    Working on input form

--- src/client/js/changeInputType.js
+++ src/client/js/changeInputType.js
@@ -1,15 +1,20 @@
 function changeInputArea(){
     let currentInput = document.getElementById("inputAreaType");
     let inputText = document.getElementById("inputText");
-    console.log("inputText element",inputText)
     let inputURL = document.getElementById("inputURL");
-    console.log("currentInput", currentInput.checked);
-    if(currentInput){  
+    if(currentInput.checked){  
+    // console.log("bef currentInput", currentInput.checked);
+    // console.log("bef inputText element",inputText);
+    // console.log("bef inputURL", inputURL);
         inputText.style.display = "none";
         inputURL.style.display = "block";
-    }
+    } else if(!currentInput.checked) {
     inputText.style.display = "block";
     inputURL.style.display = "none";
+    }
+    // console.log("aft currentInput", currentInput.checked);
+    // console.log("aft inputText element",inputText);
+    // console.log("aft inputURL", inputURL);
     // let currentShow = document.getElementById("")
 }
 
